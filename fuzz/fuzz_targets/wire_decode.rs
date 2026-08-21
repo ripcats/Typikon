2198daaa2c338bd6c7cbad8e47dd1575deeeb97f
Evgeny <pm@c8evgeny.ru>
2026-08-21 15:29:22 +0300

    Expand wire decoder fuzz coverage

--- fuzz/fuzz_targets/wire_decode.rs
+++ fuzz/fuzz_targets/wire_decode.rs
@@ -2,13 +2,44 @@
 
 use libfuzzer_sys::fuzz_target;
 use std::collections::BTreeMap;
-use typikon::{Decoder, WireCodec, MAX_PACKET_SIZE};
+use typikon::{CID_BYTES, Decoder, MAX_PACKET_SIZE, WireCodec};
 
 fuzz_target!(|data: &[u8]| {
-    if let Ok(mut decoder) = Decoder::new(data, MAX_PACKET_SIZE) {
-        let _ = decoder.varint();
-        let _ = decoder.bytes();
-        let _ = Vec::<u16>::decode(&mut decoder);
-        let _ = BTreeMap::<u8, u8>::decode(&mut decoder);
+    for operation in 0..10 {
+        let Ok(mut decoder) = Decoder::new(data, MAX_PACKET_SIZE) else {
+            return;
+        };
+        match operation {
+            0 => {
+                let _ = decoder.varint();
+            }
+            1 => {
+                let _ = decoder.bytes();
+            }
+            2 => {
+                let _ = decoder.bytes_borrowed();
+            }
+            3 => {
+                let _ = decoder.string_borrowed();
+            }
+            4 => {
+                let _ = decoder.read_cid_bytes();
+            }
+            5 => {
+                let _ = decoder.expect_cid_bytes(&[0; CID_BYTES]);
+            }
+            6 => {
+                let _ = Vec::<u16>::decode(&mut decoder);
+            }
+            7 => {
+                let _ = Vec::<String>::decode(&mut decoder);
+            }
+            8 => {
+                let _ = BTreeMap::<u8, u8>::decode(&mut decoder);
+            }
+            _ => {
+                let _ = BTreeMap::<String, Vec<u64>>::decode(&mut decoder);
+            }
+        }
     }
 });
